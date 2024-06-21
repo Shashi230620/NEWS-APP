5d3b0f17aeb557dc10ec236b1777a96307d0d065
Shashi230620 <shashi.bhatt2306@gmail.com>
2024-06-21 23:36:53 +0530

    Add files via upload
    first commit

---
+++ news/src/Sppiner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import spi from "./spiner.gif"
+const Sppiner=()=>{
+    return(<>
+    <div className="loading_Spinner">
+    <img src={spi}></img>
+    </div>
+        </>)
+}
+export default Sppiner;
